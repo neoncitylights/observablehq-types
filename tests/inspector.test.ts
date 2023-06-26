d8e1c80d08bb36bcb62d239a3c52f8c5caec79c1
Samantha <contact@samanthanguyen.me>
2023-06-25 22:58:41 -0500

    chore: replace spaces with tabs

--- tests/inspector.test.ts
+++ tests/inspector.test.ts
@@ -3,43 +3,43 @@ import { Inspector } from '@observablehq/inspector';
 import './../src/inspector.d.ts';
 
 test('inspector constructor', () => {
-  expectTypeOf(Inspector).toBeConstructibleWith(document.createElement('div'))
+	expectTypeOf(Inspector).toBeConstructibleWith(document.createElement('div'))
 })
 
 test('inspector.pending()', () => {
-  let div = document.createElement('div');
-  let inspector = new Inspector(div);
+	let div = document.createElement('div');
+	let inspector = new Inspector(div);
 
-  expectTypeOf(inspector.pending()).toBeVoid();
+	expectTypeOf(inspector.pending()).toBeVoid();
 })
 
 test('inspector.fulfilled()', () => {
-  let div = document.createElement('div');
-  let inspector = new Inspector(div);
+	let div = document.createElement('div');
+	let inspector = new Inspector(div);
 
-  expectTypeOf(inspector.fulfilled(2, 'test')).toBeVoid();
+	expectTypeOf(inspector.fulfilled(2, 'test')).toBeVoid();
 })
 
 test('inspector.rejected()', () => {
-  let p = document.createElement('p');
-  let inspector = new Inspector(p);
+	let p = document.createElement('p');
+	let inspector = new Inspector(p);
 
-  expectTypeOf(inspector.rejected('an error occurred')).toBeVoid()
+	expectTypeOf(inspector.rejected('an error occurred')).toBeVoid()
 })
 
 describe('inspector.into()', () => {
-  test('to throw an error', () => {
-    expect(() => Inspector.into('test'))
-      .toThrowError('container not found')
-  })
+	test('to throw an error', () => {
+		expect(() => Inspector.into('test'))
+			.toThrowError('container not found')
+	})
 
-  test('to not throw an error', () => {
-    let uniqueId = 'thingymabob';
-  
-    let p = document.createElement('p');
-    p.setAttribute('id', uniqueId);
-    document.body.appendChild(p);
+	test('to not throw an error', () => {
+		let uniqueId = 'thingymabob';
+	
+		let p = document.createElement('p');
+		p.setAttribute('id', uniqueId);
+		document.body.appendChild(p);
 
-    expect(() => Inspector.into('#' + uniqueId)).not.toThrowError()
-  })
+		expect(() => Inspector.into('#' + uniqueId)).not.toThrowError()
+	})
 })
